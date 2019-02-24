f574c59285bbd5025734b88efa60aa32c9e11288
Qbisst <ismaelaperez@outlook.com>
2019-02-24 17:23:54 -0500

    can't get the choices to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,37 +8,37 @@ $("#start").on("click", function () {
 
 var questions = [
     {
-        question: "1.	What is the name of Jon's direwolf?",
+        question: "What is the name of Jon's direwolf?",
         choices: ["Needle", "Snow", "Ghost", "Gray"],
         answer: "Ghost"
     },
     {
-        question: "2.	Who is king of Westeros when the series begins?",
+        question: "Who is king of Westeros when the series begins?",
         choices: ["Joffrey Baratheon", "Eddard Stark", "Petyr Baelish", "Robert Baratheon"],
         answer: "Robert Baratheon"
     },
     {
-        question: "3.	In season 2, who does Tyrion tell Varys he is planning on marrying to Princess Myrcella?",
+        question: "In season 2, who does Tyrion tell Varys he is planning on marrying to Princess Myrcella?",
         choices: ["Theon Greyjoy", "Robin Arryn", "Gregor Glegane", "Roose Baratheon"],
         answer: "Theon Greyjoy"
     },
     {
-        question: "4.	What noble house is Catelyn Stark from?",
+        question: "What noble house is Catelyn Stark from?",
         choices: ["House Bolton", "House Tully", "House Baratheon", "House Lannister"],
         answer: "House Tully"
     },
     {
-        question: "5.	Brienne of Tarth pledged her alliance to which of these pairs??",
+        question: "Brienne of Tarth pledged her alliance to which of these pairs??",
         choices: ["Renly Baratheon and Melisandre", "Sansa Stark and Selwyn Tarth", "Stannis Baratheon and Catelyn Stark", "Catelyn Stark and Renly Baratheon"],
         answer: "Catelyn Stark and Renly Baratheon"
     },
     {
-        question: "6.	What is that name of Robb Stark's wife?",
+        question: "What is that name of Robb Stark's wife?",
         choices: ["Helena", "Bethany", "Talisa", "Meera"],
         answer: "Talisa"
     },
     {
-        question: "7.	What is the name of Arya Stark's sword?",
+        question: "What is the name of Arya Stark's sword?",
         choices: ["Wrait", "Gjallarhorn", "Thorn", "Needle"],
         answer: "Needle"
     },
@@ -50,20 +50,22 @@ var game = {
 
     correct: 0,
     incorrect: 0,
-    counter: 90,
+    counter: 20,
+    //countdown timer method//
     countdown: function () {
-        game.conter--;
+        game.counter--;
         $("#counter").html(game.counter);
         if (game.counter <= 0) {
             console.log("out of time")
-            game.done();
+            game.finished();
         }
 
     },
 
     start: function () {
+        //sets the timer into html document//
         timer = setInterval(game.countdown, 1000);
-        $("#swrapper").prepend("<")
+        $("#swrapper").prepend("<h2>Time left: <span id='counter'>60</span> Seconds</h2>");
         $("#start").remove(); //<--this removes the start button, this prevents the player from generating more questions//
 
         //for loops that append the questions into an h2 tag, and the choices array with a radio button//
@@ -74,6 +76,85 @@ var game = {
             }
 
         }
+
+
+    },
+    //checks for the correct answers in each question//
+    finished: function () {
+        $.each($('input[name="question-0"]:checked'), function () {
+            if ($(this).val() == questions[0].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-1"]:checked'), function () {
+            if ($(this).val() == questions[1].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-2"]:checked'), function () {
+            if ($(this).val() == questions[2].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-3"]:checked'), function () {
+            if ($(this).val() == questions[3].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-4"]:checked'), function () {
+            if ($(this).val() == questions[4].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-5"]:checked'), function () {
+            if ($(this).val() == questions[5].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($('input[name="question-6"]:checked'), function () {
+            if ($(this).val() == questions[6].answer) {
+                game.correct++;
+            }
+            else {
+                game.incorrect++;
+            }
+
+        });
+
+        this.result(); //calls the results screen//
+    },
+
+    result: function () {
+        clearInterval(timer);
+        $("#swrapper h2").remove();
+        $("#swrapper").html("<h1>Finished!</h1>");
+        $("#swrapper").append("<h3>Questions that you answered correctly: " + this.correct + "</h3>");
+        $("#swrapper").append("<h3>Questions that you answererd incorrectly: " + this.incorrect + "</h3>");
+        $("#swrapper").append("<h3>Questions not answered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
 
 
     }
