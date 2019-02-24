df32cb730efe2d325ec0b4d53abe2e7293735f38
Qbisst <ismaelaperez@outlook.com>
2019-02-24 13:00:08 -0500

    start button works!

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//JQuery event that references the <button> id "start"//
+$("#start").on("click",function(){
+    // console.log("button press works!")
+   $("#swrapper").remove(); // this removes the secondary wrapper div when the button is pressed, this makes the button dissapear//
+})
